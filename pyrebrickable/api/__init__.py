a64ca6cf7dbaea113652b8cb76696a9add3d1fcd
rienafairefr <rienafairefr@gmail.com>
2018-01-28 12:22:33 +0100

    swagger codegen

---
+++ pyrebrickable/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pyrebrickable.api.lego_api import LegoApi
+from pyrebrickable.api.users_api import UsersApi
