8cdd5e543db529b1913578285baaf13d765399f3
Unknown <rienafairefr@gmail.com>
2018-01-29 12:58:53 +0100

    better codegen shell script avoinding stale api/, docs/, models/ docker user non-root

--- gen_openapi.sh
+++ gen_openapi.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate  -i swagger.json -l python -o /local -DprojectName=pyrebrickable -DpackageName=rebrickable
+rm -rf docs
+rm -rf rebrickable/api
+rm -rf rebrickable/models
+docker run --rm --user `id -u`:`id -g` -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i /local/swagger.json -l python -o /local -DprojectName=pyrebrickable -DpackageName=rebrickable
