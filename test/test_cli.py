db4b4f665256fe22f3758b82f30cfbbb36ab3ff3
Unknown <rienafairefr@gmail.com>
2018-01-30 11:14:16 +0100

    tests that the right api method is called

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `pyrebrickable` package."""
+from unittest import mock
+from unittest.mock import MagicMock
 
+import click
 from click.testing import CliRunner
 
-from rebrickable import cli
+from rebrickable import cli, LegoApi, UsersApi
+from inspect import getmembers
 
 
 def test_command_line_interface():
@@ -17,3 +21,30 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_command_line_interface_api_calls():
+    alldefs = getmembers(cli)
+    runner = CliRunner()
+    for (name, fun) in alldefs:
+        if name == 'register':
+            continue
+        if name == 'users_login':
+            continue
+        mocked_call = MagicMock()
+
+        if isinstance(fun, click.Command) and not isinstance(fun, click.Group):
+            if name.startswith('users'):
+                api_class = 'rebrickable.api.users_api.UsersApi',UsersApi
+            elif name.startswith('lego'):
+                api_class = 'rebrickable.api.lego_api.LegoApi', LegoApi
+
+            with mock.patch(api_class[0], spec=api_class[1]) as api_mock:
+                setattr(api_mock,name, mocked_call)
+                args = ['0' for param in fun.params]
+                result = runner.invoke(fun, obj=api_mock, args=args)
+                print(result)
+                assert mocked_call.called
+
+            pass
+    pass
