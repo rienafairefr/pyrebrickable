94cbdda955bc4128b40ff70f7f7c9b26e9585bf2
rienafairefr <rienafairefr@gmail.com>
2018-01-30 21:29:42 +0100

    gh-pages generate doc

--- api_codegen.py
+++ api_codegen.py
@@ -152,6 +152,7 @@ Some endpoints might not work, don't hesitate to file an issue<br>
 
     os.system('docker run --rm --user `id -u`:`id -g` -v ${PWD}:/local swaggerapi/swagger-codegen-cli'
               ' generate -i /local/swagger.json '
+              '-t /local/templates '
               '--git-user-id rienafairefr '
               '--git-repo-id pyrebrickable '
               '-l python -o /local -DprojectName=pyrebrickable -DpackageName=rebrickable')
--- gen_openapi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf docs
-rm -rf rebrickable/api
-rm -rf rebrickable/models
-docker run --rm --user `id -u`:`id -g` -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i /local/swagger.json -l python -o /local -DprojectName=pyrebrickable -DpackageName=rebrickable
