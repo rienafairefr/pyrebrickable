99a6197e2a3436dc7ce5e4182587635f567a1f92
rienafairefr <rienafairefr@gmail.com>
2018-03-24 13:20:06 +0100

    swagger_spec_validator

--- validate_spec.sh
+++ validate_spec.sh
@@ -2,10 +2,5 @@
 set -e
 FULL_SPEC_PATH=`readlink -f $1`
 
-# Idempotent, local library install
-virtualenv venv
-source venv/bin/activate
-pip install swagger-spec-validator
-
 # Test the Spec
 python -c "from swagger_spec_validator import validate_spec_url; validate_spec_url('file://${FULL_SPEC_PATH}')"
